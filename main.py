db15c2c553c32d587fb3c16fd2943ca341a3686b
DavidsDvm <davids10.dvm@gmail.com>
2021-10-23 22:06:09 -0500

    creation of the executable lines and auto db creator

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import render_template
 from app import create_app
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import os
+
+# Crear base de datos si esta no existe
+engine = create_engine("mariadb+mariadbconnector://root:@127.0.0.1:3306/SenaSoft")
+if not database_exists(engine.url):
+    create_database(engine.url)
+    os.system('python dbCreator.py')
 
 app = create_app()
 
@@ -13,4 +22,5 @@ def not_found(error):
 def index():
     return render_template('index.html')
 
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
