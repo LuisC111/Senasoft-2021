79c1497f9703802beb2bacc80f05f7e2ba00d43b
DavidsDvm <davids10.dvm@gmail.com>
2021-10-25 15:41:11 -0500

    new conexion to db on other container

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 #Creacion de configuraciones globales del proyecto
 class Config():
     SECRET_KEY = 'ASDGHJKLDFGHJK'
-    SQLALCHEMY_DATABASE_URI = 'mariadb+mariadbconnector://root:''@127.0.0.1:3306/SenaSoft' #Conexion con db
+    SQLALCHEMY_DATABASE_URI = 'mariadb+mariadbconnector://root:root@mysqldb/SenaSoft' #Conexion con db
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECURITY_EMAIL_SENDER = 'flowydomain@gmail.com'
 
